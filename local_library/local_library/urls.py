db112ff377eb823363a345d7e1ea7b765ca8dd40
Ted Dunn <tldunn451@gmail.com>
2018-07-09 11:14:54 -0400

    Update settings.py file and add initial url routes in local_library/urls.py

--- local_library/local_library/urls.py
+++ local_library/local_library/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls import include
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
     path('admin/', admin.site.urls),
-]
+    path('catalog/', include('catalog.urls')),
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
